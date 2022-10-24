d06c82ecabf6752e77929238cae9ee2fe9ba70a6
Oleh Kutnyi <105974562+Oleh-Kutnyi@users.noreply.github.com>
2022-10-24 22:35:03 +0300

    fix

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const input = document.getElementById("validation-input");
 
 input.addEventListener("blur", (event) => {
   const { dataset, value } = event.target;
-  if (value.length < +dataset.length) {
+  if (value.length !== +dataset.length) {
     event.target.classList.remove("valid");
     event.target.classList.add("invalid");
   } else {
